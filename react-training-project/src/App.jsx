7aaba75ee41cf3ac1b94b93855526895bac96f53
KRISH826 <krishnendupanj@gmail.com>
2023-12-13 22:02:05 +0530

    tailwind css and props

--- react-training-project/src/App.jsx
+++ react-training-project/src/App.jsx
@@ -2,9 +2,15 @@
 
 import { useState } from "react";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   let [counter, setcounter] = useState(0);
+
+  let cardDetails = {
+    des: "hi i am krishnendu sekhar panja",
+    btnname: "see profile",
+  };
 
   const valueAdd = () => {
     setcounter(counter + 1);
@@ -18,12 +24,13 @@ function App() {
   };
   return (
     <>
-      <h2>HI REACT</h2>
-      <h3>Count is {counter}</h3>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      {/* <h3 className='my-3'>Count is {counter}</h3>
       <button style={{ marginRight: 10 }} onClick={valueAdd}>
         Add
       </button>
-      <button onClick={valueRemove}>Remove</button>
+      <button onClick={valueRemove}>Remove</button> */}
+      <Card title='Title One' details={cardDetails} />
     </>
   );
 }
